785a872d9d66bb2a78a3f3ef90fee855c995b753
David Slusser <dslusser@box.com>
2017-11-19 16:57:14 -0800

    inital schema

--- matcher/admin.py
+++ matcher/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from models import (School, SchoolContact, SchoolSurvey, Policies, Expense,
+                    Raiser, RaiserSurvey)
+
 # Register your models here.
+admin.site.register(School)
+admin.site.register(SchoolContact)
+admin.site.register(SchoolSurvey)
+admin.site.register(Policies)
+admin.site.register(Expense)
+admin.site.register(Raiser)
+admin.site.register(RaiserSurvey)
+
--- matcher/models.py
+++ matcher/models.py
@@ -3,8 +3,56 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class SurveyResponse(models.Model):
-    """ survey questions """
+class GenericContact(models.Model):
+    """ Abstract table for generic contacts """
+    first_name = models.CharField(max_length=16, help_text="first name")
+    last_name = models.CharField(max_length=16, help_text="last name")
+    phone = models.CharField(max_length=16, help_text="phone number")
+    email = models.EmailField(max_length=64, help_text="email address")
+    address_1 = models.CharField(max_length=32, help_text="address line 1")
+    address_2 = models.CharField(max_length=32, blank=True, null=True, help_text="address line 2")
+    city = models.CharField(max_length=32, help_text="city")
+    state = models.CharField(max_length=16, help_text="state")
+    country = models.CharField(max_length=16, help_text="country")
+
+    class Meta:
+        abstract = True
+
+
+class School(models.Model):
+    """ table for service schools """
+    name = models.CharField(max_length=64, help_text="name of service school")
+    url = models.URLField(max_length=64, help_text="website")
+    address_1 = models.CharField(max_length=32, help_text="address line 1")
+    address_2 = models.CharField(max_length=32, blank=True, null=True, help_text="address line 2")
+    city = models.CharField(max_length=32, help_text="city")
+    state = models.CharField(max_length=16, help_text="state")
+    country = models.CharField(max_length=16, help_text="country")
+
+
+class SchoolContact(GenericContact):
+    """ contact fields specific to schools """
+    school = models.ForeignKey(School)
+    role = models.CharField(max_length=16, help_text="role or title")
+
+
+class Expense(models.Model):
+    """ table for raising expense """
+    school = models.ForeignKey(School)
+    name = models.CharField(max_length=16, help_text="name of expense")
+    percent = models.IntegerField(help_text="percent that the school covers")
+
+
+class Policies(models.Model):
+    """ table for school policies """
+    school = models.ForeignKey(School)
+    name = models.CharField(max_length=16, help_text="name of policy")
+    link = models.URLField(help_text="link to policy URL/document", blank=True, null=True)
+
+
+class SchoolSurvey(models.Model):
+    """ raiser requirements from school """
+    school = models.OneToOneField(School)
     q1 = models.CharField(max_length=16, help_text="")
     q2 = models.CharField(max_length=16, help_text="")
     q3 = models.CharField(max_length=16, help_text="")
@@ -12,23 +60,16 @@ class SurveyResponse(models.Model):
     q5 = models.CharField(max_length=16, help_text="")
 
 
-class ServiceSchool(models.Model):
-    """ table for serivce school listings """
-    name = models.CharField(max_length=64, help="name of service school")
+class Raiser(GenericContact):
+    """ table for raisers """
+    experience = models.IntegerField(blank=True, null=True, help_text="")
 
 
-class ServiceSchoolResponse(models.Model):
-    """ table for service school questions """
-    school = models.ForeignKey(ServiceSchool)
-    survey = models.ForeignKey(SurveyResponse)
-
-
-class Raiser(models.Model):
-    """ table for potential raisers """
-    name = models.CharField(max_length=64, help="name of raiser")
-
-
-class RaiserSurveyResponse(models.Model):
-    """ table to track raiser survey responses """
+class RaiserSurvey(models.Model):
+    """ survey responses from potential raisers """
     raiser = models.ForeignKey(Raiser)
-    survey = models.ForeignKey(SurveyResponse)
+    q1 = models.CharField(max_length=16, help_text="")
+    q2 = models.CharField(max_length=16, help_text="")
+    q3 = models.CharField(max_length=16, help_text="")
+    q4 = models.CharField(max_length=16, help_text="")
+    q5 = models.CharField(max_length=16, help_text="")
